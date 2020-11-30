8629f5073290879d9d8746f6f58c3bce1b255034
ikennobunga <ikennobunga@users.noreply.github.com>
2020-11-30 23:01:11 +0300

    initial commit

---
+++ src/components/global/global.js
@@ -0,0 +1,4 @@
+import {NavBar} from '../../components/global/nav/navBar'
+import {Footer} from '../../components/global/footer/footer'
+
+export {NavBar, Footer}
---
+++ src/components/global/nav/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <p>am nav-bar</p>
+    </nav>
+  )
+}
+
+export {NavBar}
