cc9a62e485cb0fa322627a1f3afd8ec644b93d81
ikennobunga <ikennobunga@users.noreply.github.com>
2020-11-30 23:29:35 +0300

    minor changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import {NavBar, Footer} from '../components/global/global'
+import {NavBar} from '../components/global/global'
 
 
 const Layout = ({ children }) => {
